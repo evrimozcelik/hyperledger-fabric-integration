c21454a374399c97c7ad9e32b89a2667aa2d9704
Evrim <evrim@tr.ibm.com>
2018-03-12 22:52:56 +0300

    Initial commit "

---
+++ com.mycompany.fabric-integration/src/main/java/com/mycompany/services/FabricController.java
@@ -0,0 +1,14 @@
+package com.mycompany.services;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FabricController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hellow";
+	}
+
+}
